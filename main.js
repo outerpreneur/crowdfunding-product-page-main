c3e242ac3b169be1d6be31aacb78b5c160b7c0c7
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-29 09:09:24 +0100

    fixing modals

--- main.js
+++ main.js
@@ -3,21 +3,35 @@ const backingBtn = document.querySelector(".backing-btn")
 const selectionModal = document.querySelector(".selection-modal")
 const closeModal = document.querySelector(".close-modal")
 
+const mobileMenuIcon = document.querySelector(".mobile-menu-icon")
+const mobileMenu = document.querySelector(".menu-mobile")
+const mobileMenuClose = document.querySelector(".mobile-menu-close")
+
 // functions
 
 function showModal1() {
     selectionModal.showModal()
-    console.log('is it working?')
+    console.log("works 2")
 }
 
 function closeModal1() {
     selectionModal.close()
 }
 
+function showMenu() {
+    mobileMenu.showModal()
+    mobileMenuIcon.style.display = "none"
+    mobileMenuClose.style.display = "block"
+}
+
+function closeMenu() {
+    mobileMenu.close()
+    mobileMenuIcon.style.display = "block"
+}
+
+// triggers
+
 backingBtn.addEventListener('click', showModal1 )
 closeModal.addEventListener('click', closeModal1)
-
-// backingBtn.addEventListener('click', () => {
-//     selectionModal.showModal()
-//     console.log('work mothafucka')
-// })
+mobileMenuIcon.addEventListener("click", showMenu)
+mobileMenuClose.addEventListener("click", closeMenu)
