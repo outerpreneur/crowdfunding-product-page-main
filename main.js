8d6b05471c59ce8135313d406db53644f93ab030
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-30 14:03:59 +0100

    add counters javascript

--- main.js
+++ main.js
@@ -21,6 +21,48 @@ const bookmarkGroup = document.querySelector('.bookmark-gp')
 const bookmarkBtn = document.querySelector('.bookmark-btn')
 const bookmarkImg = document.querySelector('.bookmark-img')
 
+// items
+
+const item1 = document.querySelector("#item-1 .type-selection");
+const item2 = document.querySelector("#item-2 .type-selection");
+const item3 = document.querySelector("#item-3 .type-selection");
+const item4 = document.querySelector("#item-4 .type-selection");
+
+
+// pledges
+
+const enterPledge1 = document.querySelector("#item-1 .enter-pledge")
+const enterPledge2 = document.querySelector("#item-2 .enter-pledge")
+const enterPledge3 = document.querySelector("#item-3 .enter-pledge")
+const enterPledge4 = document.querySelector("#item-4 .enter-pledge")
+
+// Thank you modal
+
+const thankYouModal = document.querySelector(".success-modal")
+const successModalClose = document.querySelector(".success-modal-close")
+
+
+// counters
+
+const NumberBackers = document.getElementById("pledge-counter")
+let NumberOfBackers = 5007
+const moneyRaisedCounter = document.getElementById('money-raised-counter')
+let TotalMoneyRaised = 89914
+
+// pledge input
+
+const pledgeInput1 = document.querySelector("#item-1 .pledge-input")
+const pledgeInput2 = document.querySelector("#item-2 .pledge-input")
+const pledgeInput3 = document.querySelector("#item-3 .pledge-input")
+const pledgeInput4 = document.querySelector("#item-4 .pledge-input")
+
+// continue Button
+const continueBtn1 = document.querySelector("#item-1 .continue-btn")
+const continueBtn2 = document.querySelector("#item-2 .continue-btn")
+const continueBtn3 = document.querySelector("#item-3 .continue-btn")
+const continueBtn4 = document.querySelector("#item-4 .continue-btn")
+
+
 
 // functions
 
@@ -50,6 +92,41 @@ function BookmarkBookmarked() {
     
 }
 
+function showPledge2() {
+    enterPledge3.classList.add('hidden')
+    enterPledge2.classList.remove('hidden')
+}
+
+function showPledge3() {
+    enterPledge2.classList.add('hidden')
+    enterPledge3.classList.remove('hidden')
+}
+
+function showThankYou() {
+    thankYouModal.showModal()
+    addOneBacker()
+}
+
+function closeThankYouModal() {
+    selectionModal.close()
+    thankYouModal.close()
+}
+
+function addOneBacker() {
+     NumberOfBackers += 1
+     console.log(NumberOfBackers)
+     NumberBackers.innerText = NumberOfBackers.toLocaleString('en-US')
+}
+
+function addValueToRaiseTotal() {
+    let valueInput2 = pledgeInput2.value
+    let convertToNumber = parseFloat(valueInput2)
+    let newTotalMoneyRaised = TotalMoneyRaised + convertToNumber
+    event.preventDefault()
+    moneyRaisedCounter.innerText = newTotalMoneyRaised.toLocaleString('en-US')
+    showThankYou()
+}
+
 
 // triggers
 
@@ -58,6 +135,11 @@ closeModal.addEventListener('click', closeModal1)
 mobileMenuIcon.addEventListener("click", showMenu)
 mobileMenuClose.addEventListener("click", closeMenu)
 bookmarkGroup.addEventListener("click", BookmarkBookmarked)
+item1.addEventListener("click", showThankYou)
+item2.addEventListener('change', showPledge2)
+item3.addEventListener('change', showPledge3)
+successModalClose.addEventListener('click', closeThankYouModal)
+continueBtn2.addEventListener('click', addValueToRaiseTotal )
 
 
 // Loops
@@ -67,10 +149,3 @@ selectReward.forEach(reward => {
         selectionModal.showModal()
     })
 });
-
-typeSelection.forEach(selection => {
-    selection.addEventListener('change', () => {
-        console.log("clicked")
-        enterPledge.classList.remove('hidden')
-    })
-})
